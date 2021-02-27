9705780199e8eb400cf39b0d7c66a372051dd67f
Silvan Batistella <silvan.batistella@gmail.com>
2021-02-27 19:24:34 -0300

    view de editar contatos

--- front/front-agenda/src/app/components/contact-details/contact-details.component.ts
+++ front/front-agenda/src/app/components/contact-details/contact-details.component.ts
@@ -1,4 +1,7 @@
+import { Contact } from './../../models/contact.model';
+import { AgendaService } from './../../services/agenda.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-details',
@@ -6,10 +9,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-details.component.css']
 })
 export class ContactDetailsComponent implements OnInit {
+  contact: Contact = {
+    id: 0,
+    idade: 0,
+    nome: '',
+    email: ''
+  };
+  message = '';
 
-  constructor() { }
+  constructor(
+    private agendaService: AgendaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getContact(this.route.snapshot.params.id);
+  }
+
+  getContact(id: number): void {
+    this.agendaService.get(id)
+      .subscribe(
+        data => {
+          this.contact = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updatePublished(): void {
+    const data = {
+      nome: this.contact.nome,
+      idade: this.contact.idade,
+      email: this.contact.email
+    };
+
+    this.agendaService.update(this.contact.id, data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = response.message;
+          this.router.navigate(['/contatos/']);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  deleteTutorial(): void {
+    this.agendaService.delete(this.contact.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/contatos/']);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
