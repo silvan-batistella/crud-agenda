902d2704667d5fc23c792cfc6a607c42d440c0c1
Silvan Batistella <silvan.batistella@gmail.com>
2021-02-27 19:21:28 -0300

    correção de rotas

--- front/front-agenda/src/app/app-routing.module.ts
+++ front/front-agenda/src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'contacts', pathMatch:'full'},
-  {path: 'add', component: AddContactComponent},
-  {path: 'contacts', component: ContactsListComponent},
-  {path: 'contacts/:id', component: ContactDetailsComponent}
+  {path: '', redirectTo: 'contatos', pathMatch: 'full'},
+  {path: 'contatos/add', component: AddContactComponent},
+  {path: 'contatos', component: ContactsListComponent},
+  {path: 'contatos/:id', component: ContactDetailsComponent}
 ];
 
 @NgModule({
