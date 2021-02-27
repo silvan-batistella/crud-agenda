319fcc5e0fed5e9246c0de7ca4dfdfaaac8ae328
Silvan Batistella <silvan.batistella@gmail.com>
2021-02-27 19:24:08 -0300

    view de adicionar novo contato

--- front/front-agenda/src/app/components/add-contact/add-contact.component.ts
+++ front/front-agenda/src/app/components/add-contact/add-contact.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { Contact } from './../../models/contact.model';
+import { AgendaService } from './../../services/agenda.service';
 
 @Component({
   selector: 'app-add-contact',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddContactComponent implements OnInit {
 
-  constructor() { }
+  contact : Contact = {
+    nome: '',
+    email: '',
+    idade: 0
+  };
+
+  submited = false;
+
+  constructor(
+    private agendaService: AgendaService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  saveContact(): void {
+    const data = {
+      nome: this.contact.nome,
+      email: this.contact.email,
+      idade: this.contact.idade
+    }
+    this.agendaService.create(data).subscribe(
+      response => {
+        console.log("Contato criado: (" + Date.now() +") = " + data);
+        this.router.navigate(['/contatos/']);
+      } , error => {
+        console.error("Problemas ao tentar criar o registro (" + data + ")!");
+      }
+    )
+  }
+
+  newContact() : void {
+    this.submited = false,
+    this.contact = {
+      nome: '',
+      email: '',
+      idade: 0
+    }
+  }
 }
