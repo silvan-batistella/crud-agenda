6d1dadf2b78f5de2ead8c838298b80c6aff93a66
Silvan Batistella <silvan.batistella@gmail.com>
2021-02-27 19:26:01 -0300

    Correção para CORS - visualização em ambiente de DEV

--- src/main/java/dev/batistella/agenda/AgendaApplication.java
+++ src/main/java/dev/batistella/agenda/AgendaApplication.java
@@ -2,6 +2,10 @@ package dev.batistella.agenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class AgendaApplication {
@@ -10,4 +14,16 @@ public class AgendaApplication {
 		SpringApplication.run(AgendaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+						.allowedHeaders("Authorization", "Cache-Control", "Content-Type", "Accept", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
+						.exposedHeaders("Access-Control-Expose-Headers", "Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin");;
+			}
+		};
+	}
 }
