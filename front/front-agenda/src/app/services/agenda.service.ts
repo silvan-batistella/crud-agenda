ae29e4b24d587f8fd146e838dddef8c2053c2236
Silvan Batistella <silvan.batistella@gmail.com>
2021-02-27 12:25:50 -0300

    criadas requisições no services do Angular

--- front/front-agenda/src/app/services/agenda.service.ts
+++ front/front-agenda/src/app/services/agenda.service.ts
@@ -1,9 +1,35 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { Contact } from './../models/contact.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+const baseUrl = 'http://localhost:8080/contatos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AgendaService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getAll() : Observable<Contact[]> {
+    return this.http.get<Contact[]>(baseUrl);
+  }
+
+  get (id: any) : Observable<Contact> {
+    return this.http.get<Contact>('${baseUrl}/${id}' )
+  }
+
+  create(data: any) : Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update (id: any, data: any) : Observable<any> {
+    return this.http.put('${baseUrl}/${id}', data);
+  }
+
+  delete (id: any) : Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
 }
