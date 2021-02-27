d305ebdf5b64b0f1353e5b67906bcf43316f9cbe
Silvan Batistella <silvan.batistella@gmail.com>
2021-02-27 11:00:13 -0300

    criado repository para contato

---
+++ src/main/java/dev/batistella/agenda/repository/ContatoRepository.java
@@ -0,0 +1,9 @@
+package dev.batistella.agenda.repository;
+
+import dev.batistella.agenda.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
+}
